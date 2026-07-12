f3ee7eef674cec56b3371168fb2c92cc17ea4f5d
Maciej Szreder <mszreder3.14@wp.pl>
2026-07-12 19:31:42 +0200

    fps dev tool

--- dev/tools.js
+++ dev/tools.js
@@ -1,5 +1,7 @@
 import * as gui from '../gui/interface.js';
 import {tapeDecode} from '../logic/tape.js';
+import render from '../gui/render.js';
+import color from '../gui/colors.js';
 
 function serialize(tapeEncoded)
 {
@@ -20,4 +22,28 @@ export function get_service_tape_content()
 export function get_output_tape_content()
 {
 	return serialize(gui.outputTape.source.value);
+}
+
+export function render_fps()
+{
+	render(new Fps);
+}
+
+class Fps
+{
+	start = Date.now();
+
+	font = '50px sans-serif';
+	textColor = color.light;
+	x = 0;
+	y = 50;
+
+	draw(ctx)
+	{
+		let fps = Date.now() - this.start;
+		ctx.font = this.font;
+		ctx.fillStyle = this.textColor;
+		ctx.fillText((1000/fps).toFixed(2)+" fps", this.x, this.y);
+		this.start = Date.now();
+	}
 }
