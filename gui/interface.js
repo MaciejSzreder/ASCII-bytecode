477ff166567857de1d7cf8ba445a79b3fec5ca40
Maciej Szreder <mszreder3.14@wp.pl>
2026-07-15 05:41:34 +0200

    do not place object outside of canvas

--- gui/interface.js
+++ gui/interface.js
@@ -9,16 +9,26 @@ const tapeComputerGap = tapeGap;
 
 export let serviceTape, inputTape, outputTape, computer;
 
+function positionClamp(position, width, max){
+	if(position < 0){
+		return 0;
+	}
+	if(position+width <= max){
+		return position;
+	}
+	return max-width;
+}
+
 document.addEventListener('DOMContentLoaded', ()=>{
 	let view = HtmlId`html`;
 	let main = HtmlId`main`;
 	main.style.height = main.height = view.clientHeight;
-	main.style.width = main.width = view.clientWidth;
+	let width = main.style.width = main.width = view.clientWidth;
 	
 	render(serviceTape = new Tape(HtmlId`serviceInput`, 0));
-	render(inputTape = new Tape(HtmlId`input`, Tape.width + tapeGap));
-	render(computer = new Computer(2*Tape.width + 2*tapeGap,0, ()=>image));
-	render(outputTape = new Tape(HtmlId`output`, 2*Tape.width + tapeGap + 2*tapeComputerGap + Computer.width));
+	render(inputTape = new Tape(HtmlId`input`, positionClamp( Tape.width + tapeGap, Tape.width, width)));
+	render(computer = new Computer(positionClamp(2*Tape.width + 2*tapeGap, Computer.width, width), 0, ()=>image));
+	render(outputTape = new Tape(HtmlId`output`, positionClamp(2*Tape.width + tapeGap + 2*tapeComputerGap + Computer.width, Tape.width, width)));
 
 	computer.connectServiceInput(HtmlId`serviceInput`);
 	computer.connectInput(HtmlId`input`);
